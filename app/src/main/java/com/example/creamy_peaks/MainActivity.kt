493cbf1995725ac6a1a3da0235538f83c2907fed
Yusuf Martinus Arief <brothanes11@gmail.com>
2024-04-19 20:00:48 +0700

    Adding Splash Screen into Application

--- app/src/main/java/com/example/creamy_peaks/MainActivity.kt
+++ app/src/main/java/com/example/creamy_peaks/MainActivity.kt
@@ -3,12 +3,15 @@ package com.example.creamy_peaks
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Thread.sleep(2000)
+        installSplashScreen()
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
