d81b7529d6d4c6c1a10d81e896652bdd066ef729
Yusuf Martinus Arief <brothanes11@gmail.com>
2024-04-25 19:20:13 +0700

    Update Login Page and Creating Register Page

--- app/src/main/java/com/example/creamy_peaks/MainActivity.kt
+++ app/src/main/java/com/example/creamy_peaks/MainActivity.kt
@@ -2,11 +2,8 @@ package com.example.creamy_peaks
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.creamy_peaks.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -21,21 +18,14 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.btnMasuk.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
         binding.btnDaftar.setOnClickListener {
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
         }
-
-        binding.btnMasuk.setOnClickListener {
-            val intent = Intent(this, RegisterActivity::class.java)
-            startActivity(intent)
-        }
-//        enableEdgeToEdge()
-//        setContentView(R.layout.activity_main)
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }
     }
 }
