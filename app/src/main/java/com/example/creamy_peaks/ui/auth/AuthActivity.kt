dd5cd567aa6b689f80cdcb18b8e5cb93f2001801
Yusuf Martinus Arief <brothanes11@gmail.com>
2024-05-23 19:21:32 +0700

    Redesign Login & Register Page and Fixed several bugs

--- app/src/main/java/com/example/creamy_peaks/ui/auth/AuthActivity.kt
+++ app/src/main/java/com/example/creamy_peaks/ui/auth/AuthActivity.kt
@@ -1,4 +1,4 @@
-package com.example.creamy_peaks.ui.user
+package com.example.creamy_peaks.ui.auth
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
@@ -7,15 +7,20 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.creamy_peaks.R
 
-class RegisterActivity : AppCompatActivity() {
+class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_register)
+        setContentView(R.layout.activity_auth)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragment_container, LoginFragment())
+            commit()
         }
     }
 }
--- app/src/main/java/com/example/creamy_peaks/ui/common/WelcomeFragment.kt
+++ app/src/main/java/com/example/creamy_peaks/ui/common/WelcomeFragment.kt
@@ -4,9 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import com.example.creamy_peaks.ui.user.LoginActivity
-import com.example.creamy_peaks.ui.user.RegisterActivity
 import com.example.creamy_peaks.databinding.FragmentWelcomeBinding
+import com.example.creamy_peaks.ui.auth.AuthActivity
 
 class WelcomeFragment : AppCompatActivity() {
 
@@ -20,14 +19,21 @@ class WelcomeFragment : AppCompatActivity() {
         binding = FragmentWelcomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnMasuk.setOnClickListener {
-            val intent = Intent(this@WelcomeFragment, LoginActivity::class.java)
-            startActivity(intent)
-        }
+//        binding.btnMasuk.setOnClickListener {
+//            val intent = Intent(this@WelcomeFragment, LoginActivity::class.java)
+//            startActivity(intent)
+//        }
+//
+//        binding.btnDaftar.setOnClickListener {
+//            val intent = Intent(this@WelcomeFragment, RegisterActivity::class.java)
+//            startActivity(intent)
+//        }
 
-        binding.btnDaftar.setOnClickListener {
-            val intent = Intent(this@WelcomeFragment, RegisterActivity::class.java)
-            startActivity(intent)
+        with(binding) {
+            btnMulai.setOnClickListener {
+                val intent = Intent(this@WelcomeFragment, AuthActivity::class.java)
+                startActivity(intent)
+            }
         }
     }
 }
